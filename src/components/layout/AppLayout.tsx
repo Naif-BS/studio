604319df4cd9b92e93c625a5227466a0cbef1661
Naif Abdullah <naif.azh@gmail.com>
2025-05-20 20:16:19 +0000

    fit the page content correctly

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -11,7 +11,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
       <div className="flex flex-col min-h-screen bg-background">
         <SidebarInset className="flex flex-col flex-1 overflow-hidden"> {/* Manages Header and Main Content */}
           <Header />
-          <main className="flex-1 p-4 md:p-6 lg:p-8 overflow-auto pb-20"> {/* Added pb-20 for bottom nav space */}
+          <main className="flex-1 px-4 md:px-6 lg:px-8 pt-16 pb-20 overflow-auto"> {/* Adjusted padding */}
             {children}
           </main>
         </SidebarInset>
